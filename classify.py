f6195f688e7b139db05f7a8676c17d28345c596c
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2019-11-16 14:40:41 -0500

    actually fix the orientation of the confusion matrix

--- classify.py
+++ classify.py
@@ -30,9 +30,9 @@ def plot_confusion_matrix(cm, normalize=False, title='Confusion Matrix', cmap='B
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     plt.figure(figsize=(6., 6.))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap, aspect='equal', origin='upper')
+    plt.imshow(cm, interpolation='nearest', cmap=cmap, aspect='equal')
     plt.xlim(-0.5, 4.5)
-    plt.ylim(-0.5, 4.5)
+    plt.ylim(4.5, -0.5)
     plt.title(title)
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
