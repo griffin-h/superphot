5bf40c4e98aeee3584746885a5345824b0fa5d00
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-13 17:51:05 +0000

    Update docs after building Travis build 16 of griffin-h/superphot
    The docs were built from the branch 'master' against the commit
    ba89861349eba8b1656c05b2d668f45bbc275bef.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/griffin-h/superphot/jobs/333366739.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post15+g2a59942',
+    VERSION: '0.1.0.post18+gba89861',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
